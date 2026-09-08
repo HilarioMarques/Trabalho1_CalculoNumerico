ef2e4414fe2238c5bd0f81f0627e2bd40593f9c3
Hilario Filho <hilariospol@gmail.com>
2026-09-08 20:35:49 -0300

    fazendo o método da bisseção e testando no main

---
+++ metodos/bissecao.py
@@ -0,0 +1,44 @@
+class Bissecao:
+
+    def __init__(self, funcao): #Recebe a função matemática que será usada
+        self.funcao = funcao
+
+    def resolver(self, a, b, epsilon):
+        """
+        Método da Bisseção.
+
+        Parâmetros:
+            a -> limite inferior
+            b -> limite superior
+            epsilon -> tolerância do erro
+
+        Retorna:
+            raiz aproximada
+            histórico das iterações
+        """
+
+        historico = [] #Guardando cada passo
+
+        while (b - a) / 2 > epsilon: #Quando o intervalo ficar muito pequeno, o while para
+
+            meio = (a + b) / 2 #A Bisseção sempre divide o intervalo ao meio
+
+            historico.append({
+                "a": a,
+                "b": b,
+                "xm": meio,
+                "f(xm)": self.funcao.f(meio)
+            })
+
+            #Aqui tá sendo feito o teste do sinal se no primeiro if os sinais forem diferentes 
+            #[a ----- meio] contém raiz, então descartamos a metade direita. Caso contrário
+            #[meio ----- b] então descartamos a metade esquerda
+
+            if self.funcao.f(a) * self.funcao.f(meio) < 0:
+                b = meio
+            else:
+                a = meio
+
+        raiz = (a + b) / 2
+
+        return raiz, historico
